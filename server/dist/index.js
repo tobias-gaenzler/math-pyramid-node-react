aacba8b7f5dd692950ba39bbd09acbd616b591d8
tobias-gaenzler <1056318+tobias-gaenzler@users.noreply.github.com>
2024-01-15 09:40:20 +0100

    Fix small widths

--- server/dist/index.js
+++ server/dist/index.js
@@ -5,54 +5,36 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
+const uuid_1 = require("uuid");
+const expressWs = require("express-ws");
+const path = require("path");
+const UserManager_1 = require("./user/UserManager");
+const CloseHandler_1 = require("./websocket-handler/CloseHandler");
+const ErrorHandler_1 = require("./websocket-handler/ErrorHandler");
+const MessageHandler_1 = require("./websocket-handler/MessageHandler");
 dotenv_1.default.config();
-const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
-app.get("/", (req, res) => {
-    res.send("Express + TypeScript Server");
+var expressWebSocketServer = expressWs((0, express_1.default)());
+var server = expressWebSocketServer.app;
+const userManager = new UserManager_1.UserManager();
+const closeHandler = new CloseHandler_1.CloseHandler(userManager);
+const errorHandler = new ErrorHandler_1.ErrorHandler(userManager);
+const messageHandler = new MessageHandler_1.MessageHandler(userManager, expressWebSocketServer);
+server.use(express_1.default.static(path.resolve(__dirname, '../../app/build')));
+server.ws('/', (ws) => {
+    const id = (0, uuid_1.v4)();
+    console.log(`New connection: ${id} `);
+    userManager.addUser(ws, id);
+    ws.onmessage = function (rawMessage) {
+        messageHandler.handleMessage(ws, rawMessage);
+    };
+    ws.on('error', (error) => {
+        errorHandler.handleError(ws, error);
+    });
+    ws.on("close", (code) => {
+        closeHandler.handleClose(ws, code);
+    });
 });
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`[server]: Server is running at http://localhost:${port}`);
 });
-// const path = require('path');
-// const { v4: uuidv4 } = require("uuid");
-// var express = require('express');
-// var expressWs = require('express-ws');
-// const PORT = process.env.PORT || 3000;
-// const users = new Set();
-// var expressWs = expressWs(express());
-// var app = expressWs.app;
-// app.use(express.static(path.resolve(__dirname, '../../app/build')));
-// var aWss = expressWs.getWss('/');
-// app.ws('/', function (ws, req) {
-//   ws.id = uuidv4();
-//   const userRef = {
-//     ws,
-//   };
-//   users.add(userRef);
-//   console.log(`New connection: ${ws.id} `);
-//   ws.onmessage = function (message) {
-//     console.log('received: %s', message);
-//     const data = JSON.parse(message.data);
-//     if (data.action === "username") {
-//       userRef.ws.username = data.sender;
-//       console.log(`Received username: ${data.sender} ${userRef.ws.id}`);
-//     } else if (data.action === "start") {
-//       console.log(`Received start event: ${JSON.stringify(data.data)} from ${userRef.ws.id} ${userRef.ws.username}`);
-//       aWss.clients.forEach(function (client) {
-//         client.send(`{ "size": "3", "solutionValues": [ 1, 2, 3, 4, 5, 6], "startValues": [1, 2, 3, 4, 5, 6] }`);
-//       });
-//     }
-//   };
-//   ws.on('error', (error) => {
-//     console.error(`Deleting user ${userRef.ws.id}, ${userRef.ws.username} on error ${error}`);
-//     users.delete(userRef);
-//   });
-//   ws.on("close", (code) => {
-//     users.delete(userRef);
-//     console.log(
-//       `User ${ws.username} is leaving the game.id: ${ws.id}, code: ${code}`
-//     );
-//   });
-// });
-// app.listen(PORT);

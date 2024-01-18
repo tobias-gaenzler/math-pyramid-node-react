5650131f6ed36a6b080392239bc8ef9c84528927
tobias-gaenzler <1056318+tobias-gaenzler@users.noreply.github.com>
2024-01-18 20:55:56 +0100

    Add test

--- server/src/math-pyramid/MathPyramidSolver.test.ts
+++ server/src/math-pyramid/MathPyramidSolver.test.ts
@@ -1,14 +1,7 @@
 import { MathPyramidSolver } from './MathPyramidSolver';
 
 describe('MathPyramidSolver', () => {
-    let solver: MathPyramidSolver;
-    beforeEach(() => {
-        solver = new MathPyramidSolver();
-    });
-
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
+    let solver: MathPyramidSolver = new MathPyramidSolver();
 
     test('isSolvable should return true for solvable pyramid', () => {
         // Arrange
@@ -21,6 +14,7 @@ describe('MathPyramidSolver', () => {
         // Assert
         expect(result).toBeTruthy();
     });
+
     test('isSolvable should return false for not uniquely solvable pyramid', () => {
         // Arrange
         const startValues = new Map<number, number>();
@@ -31,5 +25,15 @@ describe('MathPyramidSolver', () => {
         const result = solver.isSolvable(startValues, 3);
         // Assert
         expect(result).toBeFalsy();
+    });
+
+    test('solveBottomUp should return solution', () => {
+        // Arrange
+        const bottomValues: number[] = [1, 2, 3];
+        const solution: number[] = [1, 2, 3, 3, 5, 8];
+        // Act
+        const result = solver.solveBottomUp(3, bottomValues);
+        // Assert
+        expect(result).toEqual(solution);
     });
 });
